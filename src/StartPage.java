ff1c5b5e4fb5a9e379262c626f9cf2dda76791d8
marco-garro <marcogarro313@gmail.com>
2023-12-22 22:44:47 +0100

    StartPage.java implementation

---
+++ src/StartPage.java
@@ -0,0 +1,66 @@
+import DipendenteSwing.DipendenteNew;
+import Negozietti.DAO.DipendenteDAO;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+
+public class StartPage extends JFrame implements ActionListener {
+
+    JButton btnFrutto = null;
+    JButton btnDipendente = null;
+    JButton btnNegozio = null;
+
+    public  StartPage(){
+        setSize(800,600);
+        setTitle("DB Manager");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        initUI();
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    private void initUI() {
+        JPanel panel = new JPanel(new GridLayout(2,1));
+        JPanel pnlNorth = new JPanel(new BorderLayout());
+        JLabel lblTitle = new JLabel("Frutti DB Manager", SwingConstants.CENTER);
+        lblTitle.setFont(new Font("arial",Font.BOLD,30));
+        pnlNorth.add(lblTitle, BorderLayout.SOUTH);
+
+        JPanel pnlSouth = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        btnDipendente = new JButton("Dipendenti");
+        btnFrutto = new JButton("Frutto");
+        btnNegozio = new JButton("Negozio");
+        btnNegozio.addActionListener(this);
+        btnFrutto.addActionListener(this);
+        btnDipendente.addActionListener(this);
+        pnlSouth.add(btnDipendente);
+        pnlSouth.add(btnFrutto);
+        pnlSouth.add(btnNegozio);
+
+        panel.add(pnlNorth);
+        panel.add(pnlSouth);
+
+        add(panel);
+    }
+
+    public static void main(String[] args) {
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+
+                new StartPage();
+            }
+        };
+        SwingUtilities.invokeLater(r);
+    }
+}
